7c11bd318b5b71af9801c21a38e995fdbe24db71
Karthi Mol <75553966+JuiceyBeans@users.noreply.github.com>
2026-03-27 11:51:13 +0000

    Refactor mixin a bit

--- src/main/java/com/juiceybeans/leaf_me_be/mixin/GrowingPlantBlockMixin.java
+++ src/main/java/com/juiceybeans/leaf_me_be/mixin/GrowingPlantBlockMixin.java
@@ -3,21 +3,13 @@ package com.juiceybeans.leaf_me_be.mixin;
 import com.juiceybeans.leaf_me_be.ModTags;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
-import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.level.BlockGetter;
-import net.minecraft.world.level.LevelReader;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.GrowingPlantBlock;
-import net.minecraft.world.level.block.GrowingPlantHeadBlock;
 import net.minecraft.world.level.block.state.BlockState;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(GrowingPlantBlock.class)
 public abstract class GrowingPlantBlockMixin {
@@ -28,7 +20,7 @@ public abstract class GrowingPlantBlockMixin {
                     target = "Lnet/minecraft/world/level/block/state/BlockState;isFaceSturdy(Lnet/minecraft/world/level/BlockGetter;Lnet/minecraft/core/BlockPos;Lnet/minecraft/core/Direction;)Z"
             )
     )
-    private boolean i(BlockState instance, BlockGetter blockGetter, BlockPos pos, Direction direction, Operation<Boolean> original) {
+    private boolean leaf_me_be$cave_vine_placement(BlockState instance, BlockGetter blockGetter, BlockPos pos, Direction direction, Operation<Boolean> original) {
         return original.call(instance, blockGetter, pos, direction) || blockGetter.getBlockState(pos.above()).is(ModTags.SUPPORTS_CAVE_VINES);
     }
 }
